32e3b5d9d021a7952dfaa91cb51d7907dbd15177
James Knighton <iamknighton@gmail.com>
2015-04-29 18:11:47 -0700

    Add nice sprintf from gflags.

--- phraser/cc/misc/strings.cc
+++ phraser/cc/misc/strings.cc
@@ -71,4 +71,73 @@ void Trim(string* s) {
     *s = s->substr(begin, end - begin);
 }
 
-}  // namespace string
+static void InternalStringPrintf(string* output, const char* format, va_list ap) {
+    char space[128];    // try a small buffer and hope it fits
+
+    // It's possible for methods that use a va_list to invalidate
+    // the data in it upon use.  The fix is to make a copy
+    // of the structure before using it and use that copy instead.
+    va_list backup_ap;
+    va_copy(backup_ap, ap);
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wformat-nonliteral"
+    size_t bytes_written = static_cast<size_t>(vsnprintf(
+        space, sizeof(space), format, backup_ap));
+#pragma clang diagnostic pop
+    va_end(backup_ap);
+
+    if (bytes_written < sizeof(space)) {
+        output->append(space, bytes_written);
+        return;
+    }
+
+    // Repeatedly increase buffer size until it fits.
+    size_t length = sizeof(space);
+    while (true) {
+        // We need exactly "bytes_written+1" characters
+        length = bytes_written + 1;
+        char* buf = new char[length];
+
+        // Restore the va_list before we use it again
+        va_copy(backup_ap, ap);
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wformat-nonliteral"
+        bytes_written = static_cast<size_t>(vsnprintf(
+            buf, length, format, backup_ap));
+#pragma clang diagnostic pop
+        va_end(backup_ap);
+
+        if (bytes_written < length) {
+            output->append(buf, bytes_written);
+            delete [] buf;
+            return;
+        }
+        delete [] buf;
+    }
+}
+
+void SStringPrintf(string* output, const char* format, ...) {
+    va_list ap;
+    va_start(ap, format);
+    output->clear();
+    InternalStringPrintf(output, format, ap);
+    va_end(ap);
+}
+
+void StringAppendF(string* output, const char* format, ...) {
+    va_list ap;
+    va_start(ap, format);
+    InternalStringPrintf(output, format, ap);
+    va_end(ap);
+}
+
+string StringPrintf(const char* format, ...) {
+    va_list ap;
+    va_start(ap, format);
+    string output;
+    InternalStringPrintf(&output, format, ap);
+    va_end(ap);
+    return output;
+}
+
+}  // namespace strings
--- phraser/cc/misc/strings.h
+++ phraser/cc/misc/strings.h
@@ -24,6 +24,12 @@ void SplitByWhitespace(const string& s, vector<string>* v);
 // Trim both sides.
 void Trim(string* s);
 
+// Equivalents of printf that work on strings, from gflags source code.
+// Clears output before writing to it.
+void SStringPrintf(string* output, const char* format, ...);
+void StringAppendF(string* output, const char* format, ...);
+string StringPrintf(const char* format, ...);
+
 }  // namespace string
 
 #endif  // CC_MISC_STRINGS_H_

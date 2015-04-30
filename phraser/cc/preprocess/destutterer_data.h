62e933c2611fc0327a33f462144269886e8264e2
James Knighton <iamknighton@gmail.com>
2015-04-30 14:26:01 -0700

    destutterer_data

---
+++ phraser/cc/preprocess/destutterer_data.h
@@ -0,0 +1,10 @@
+#ifndef CC_PREPROCESS_DESTUTTERER_DATA_H_
+#define CC_PREPROCESS_DESTUTTERER_DATA_H_
+
+namespace destutterer_data {
+
+extern uint32_t DIGITS[];
+
+}  // namespace destutterer_data
+
+#endif  // CC_PREPROCESS_DESTUTTERER_DATA_H_

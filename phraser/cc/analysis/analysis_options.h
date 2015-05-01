965d247d41bffde43572c81575a0915f53fa9b04
James Knighton <iamknighton@gmail.com>
2015-05-01 13:19:37 -0700

    Add track_* flags to analysis options to let caller strip it down.

--- phraser/cc/analysis/analysis_options.h
+++ phraser/cc/analysis/analysis_options.h
@@ -5,14 +5,25 @@
 
 struct AnalysisOptions {
     AnalysisOptions() :
+        track_index_translation(true),
         destutter_max_consecutive(3),
+        track_chr2drop(true),
         replace_html_entities(true)
     {}
 
-    // Preprocessing.
-    size_t destutter_max_consecutive;
+    // General flags:
+    // * Map tokens to spans in the original text.
+    bool track_index_translations;
 
-    // Tokenization.
+    // Preprocessing flags:
+    // * Maximum number of consecutive code points before we start dropping them.
+    // * Whether to keep track of code point drop counts from destuttering.
+    size_t destutter_max_consecutive;
+    bool track_chr2drop;
+
+    // Tokenization flags:
+    // * Whether to replace HTML entities in the text with their Unicode
+    //   equivalents.
     bool replace_html_entities;
 };
 

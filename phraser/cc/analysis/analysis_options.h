ec09e905db4bf5698c7c8bde2a41013c428f4308
James Knighton <iamknighton@gmail.com>
2015-05-01 13:20:39 -0700

    Remove them.  Deal with them later if perf is actually an issue here.

--- phraser/cc/analysis/analysis_options.h
+++ phraser/cc/analysis/analysis_options.h
@@ -5,21 +5,13 @@
 
 struct AnalysisOptions {
     AnalysisOptions() :
-        track_index_translation(true),
         destutter_max_consecutive(3),
-        track_chr2drop(true),
         replace_html_entities(true)
     {}
 
-    // General flags:
-    // * Map tokens to spans in the original text.
-    bool track_index_translations;
-
     // Preprocessing flags:
     // * Maximum number of consecutive code points before we start dropping them.
-    // * Whether to keep track of code point drop counts from destuttering.
     size_t destutter_max_consecutive;
-    bool track_chr2drop;
 
     // Tokenization flags:
     // * Whether to replace HTML entities in the text with their Unicode

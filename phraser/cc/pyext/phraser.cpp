92e6220be06af57c03ac28dbf682a18f67e53ef9
James Knighton <iamknighton@gmail.com>
2015-05-05 12:28:25 -0700

    Implement python extension Init() passing phrase config texts.

--- phraser/cc/pyext/phraser.cpp
+++ phraser/cc/pyext/phraser.cpp
@@ -1,9 +1,18 @@
 #include <Python.h>
+#include <string>
+#include <vector>
+
+#include "cc/analysis/analyzer.h"
+
+using std::string;
+using std::vector;
 
 namespace {
 
 char PHRASER_DOC[] =
     "Python extension that detects phrases in text.\n";
+
+Analyzer* ANALYZER = NULL;
 
 char INIT_DOC[] =
     "phrase config texts -> error str or None.\n"
@@ -15,6 +24,39 @@ char INIT_DOC[] =
     "    err = phraser.Init(phrases)\n"
     "    assert not err\n";
 
+PyObject* Init(PyObject* self, PyObject* args) {
+    // Get args.
+    PyObject* list;
+    if (!PyArg_ParseTuple(args, "O!", &PyList_Type, &list)) {
+        return PyUnicode_FromString("Invalid arguments.");
+    }
+
+    // Extract phrase configs from list.
+    vector<string> phrase_configs;
+    Py_ssize_t length = PyList_Size(list);
+    for (Py_ssize_t i = 0; i < length; ++i) {
+        PyObject* s = PyList_GetItem(list, i);
+        if (!PyString_Check(s)) {
+            return PyUnicode_FromFormat(
+                "List item at index %ld was not a string.", i);
+        }
+        phrase_configs.emplace_back(PyString_AsString(s));
+    }
+
+    // Allocate the Analyzer.
+    if (!ANALYZER) {
+        ANALYZER = new Analyzer();
+    }
+
+    // Call Init().
+    string error;
+    if (!ANALYZER->Init(phrase_configs, &error)) {
+        return PyUnicode_FromString(error.data());
+    }
+
+    return Py_None;
+}
+
 char TO_D_DOC[] =
     "-> dict.\n"
     "\n"
@@ -25,6 +67,10 @@ char TO_D_DOC[] =
     "\n"
     "    d = phrasser.to_d()\n"
     "    print d\n";
+
+PyObject* ToD(PyObject* self, PyObject* args) {
+    return NULL;
+}
 
 char ANALYZE_DOC[] =
     "text, options -> phrase detection result dict, error str.\n"
@@ -39,14 +85,6 @@ char ANALYZE_DOC[] =
     "    }\n"
     "    err = phraser.Analyze(text, options)\n"
     "    assert not err\n";
-
-PyObject* Init(PyObject* self, PyObject* args) {
-    return NULL;
-}
-
-PyObject* ToD(PyObject* self, PyObject* args) {
-    return NULL;
-}
 
 PyObject* Analyze(PyObject* self, PyObject* args) {
     return NULL;

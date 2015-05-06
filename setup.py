6b8b694456122431f623822c6c0663146d81badb
James Knighton <iamknighton@gmail.com>
2015-05-06 15:19:57 -0700

    Bump version.

--- setup.py
+++ setup.py
@@ -71,7 +71,7 @@ phraser = Extension(
 
 setup(
     name='phraser',
-    version='0.0.1',
+    version='0.0.2',
     author='James Knighton',
     author_email='iamknighton@gmail.com',
     description='Detects phrases in English text',

615da6a5ec4748d7f1cdc3a439a8fd77f3f2cb95
James Knighton <iamknighton@gmail.com>
2015-05-06 14:02:17 -0700

    Make phraser installable via pip.

--- setup.py
+++ setup.py
@@ -5,6 +5,44 @@ import os
 SRC_ROOT = 'phraser/'
 
 
+# 1. Base flags.
+# 2. Max out warnings.
+# 3. Disable some warnings.
+# 4. Disable more warnings for LAPOS.
+COMPILE_FLAGS = ("""
+    -std=c++11
+    -fcolor-diagnostics
+    -O3
+    -ferror-limit=5
+    -I%s
+
+    -Wpedantic
+    -Wall
+    -Weverything
+    -Wextra
+    -Werror
+
+    -Wno-c++98-compat-pedantic
+    -Wno-covered-switch-default
+    -Wno-exit-time-destructors
+    -Wno-global-constructors
+    -Wno-padded
+    -Wno-weak-vtables
+
+    -Wno-shorten-64-to-32
+    -Wno-sign-conversion
+    -Wno-old-style-cast
+    -Wno-sign-compare
+    -Wno-float-equal
+    -Wno-unused-variable
+    -Wno-unused-parameter
+    -Wno-unused-function
+""" % SRC_ROOT).split()
+
+
+# C++ source naming convention:
+# * Main files end with .cpp
+# * Everything else ends with .cc
 def find_cc_files(root_dir):
     ff = []
     for root, dirs, files in os.walk(root_dir):
@@ -15,6 +53,7 @@ def find_cc_files(root_dir):
     return ff
 
 
+# Use clang.
 os.environ['CC'] = 'clang'
 os.environ['CXX'] = 'clang++'
 
@@ -22,6 +61,7 @@ os.environ['CXX'] = 'clang++'
 phraser = Extension(
     'phraser',
     sources=find_cc_files(SRC_ROOT) + ['phraser/cc/pyext/phraser.cpp'],
+    extra_compile_args=COMPILE_FLAGS,
     include_dirs=[SRC_ROOT],
     libraries=['boost_regex'],
     library_dirs=['/usr/local/lib/'],
@@ -29,5 +69,11 @@ phraser = Extension(
 
 
 setup(
+    name='phraser',
+    version='0.0.1',
+    author='James Knighton',
+    author_email='iamknighton@gmail.com',
+    description='Detects phrases in English text',
+    license='MIT',
     ext_modules=[phraser],
 )

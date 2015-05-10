b46ef8768dfbac5344d4d82161bb5c3dac7f0025
James Knighton <iamknighton@gmail.com>
2015-05-10 05:16:44 -0700

    Tokenize the contractions in reductions data.

---
+++ phraser/cc/frontend/reductions/reduction_data.h
@@ -0,0 +1,18 @@
+#ifndef CC_FRONTEND_REDUCTIONS_REDUCTION_DATA_H_
+#define CC_FRONTEND_REDUCTIONS_REDUCTION_DATA_H_
+
+#include <string>
+#include <vector>
+
+using std::string;
+using std::vector;
+
+// Automatically generated file -- do not edit!
+
+namespace reduction_data = {
+
+extern vector<vector<string>> reductions;
+
+}  // namespace
+
+#endif  // CC_FRONTEND_REDUCTIONS_REDUCTION_DATA_H_

033ca082e8b044000595cefbd5af7933fa4c70d9
Eugene Scherba <escherba@gmail.com>
2015-05-20 19:43:19 -0700

    fix python code

--- phraser/__init__.py
+++ phraser/__init__.py
@@ -1,37 +1,4 @@
-from phraser.phraserext import PhraserExt as ext
-
-
-def init(phrase_config_ff):
-    """
-    phrase config file names -> None.
-
-    Initialize the module with some phrase configs to match against.
-
-    Call this first.
-    """
-    phrase_configs = map(lambda f: open(f).read(), phrase_config_ff)
-    err = ext(phrase_configs)
-    if err:
-        raise err
-    return True
-
-
-def to_dict():
-    """
-    -> dict
-
-    Dump the module's state as a recursive dict.
-    """
-    return ext.to_dict()
-
-
-def to_html():
-    """
-    -> unicode
-
-    Dump the module's state as pretty HTML.
-    """
-    raise NotImplementedError
+from phraser.phraserext import PhraserExt
 
 
 class AnalysisOptions(object):
@@ -66,18 +33,24 @@ class AnalysisResult(object):
             self.phrase_matches.append(phrase)
 
 
-def analyze(text, options=None):
-    """
-    unicode -> AnalysisResult
-    unicode, AnalysisOptions -> AnalysisResult
+class Phraser(object):
 
-    Analyze the test.  Returns (a) what it did to the text and (b) phrase match
-    results.
-    """
-    assert isinstance(text, unicode)
-    if options:
-        assert isinstance(options, AnalysisOptions)
-    else:
-        options = AnalysisOptions()
-    result = ext.analyze(text, options.to_dict())
-    return AnalysisResult(result)
+    def __init__(self, phrase_config_ff):
+        phrase_configs = map(lambda f: open(f).read(), phrase_config_ff)
+        self.ext = PhraserExt(phrase_configs)
+
+    def analyze(self, text, options=None):
+        """
+        unicode -> AnalysisResult
+        unicode, AnalysisOptions -> AnalysisResult
+
+        Analyze the test.  Returns (a) what it did to the text and (b) phrase match
+        results.
+        """
+        assert isinstance(text, unicode)
+        if options:
+            assert isinstance(options, AnalysisOptions)
+        else:
+            options = AnalysisOptions()
+        result = self.ext.analyze(text, options.to_dict())
+        return AnalysisResult(result)
--- tests/test_phraser.py
+++ tests/test_phraser.py
@@ -2,20 +2,20 @@ import phraser
 import unittest
 from pkg_resources import resource_filename
 
-
 PHRASER_CONFIG = resource_filename(__name__, "./threat_statement.txt")
+
 
 class TestPhraser(unittest.TestCase):
     def test_phraser_simple(self):
-        result = phraser.init([PHRASER_CONFIG])
-        self.assertEqual(True, result)
+        runtime = phraser.Phraser([PHRASER_CONFIG])
+        self.assertTrue(bool(runtime))
         text = u'blah blah some string'
-        obj = phraser.analyze(text)
+        obj = runtime.analyze(text)
         self.assertEqual(text, obj.original_text)
 
     def test_phraser_threat1(self):
-        result = phraser.init([PHRASER_CONFIG])
-        self.assertEqual(True, result)
+        runtime = phraser.Phraser([PHRASER_CONFIG])
+        self.assertTrue(bool(runtime))
         text = u'i will kill you.'
-        obj = phraser.analyze(text)
+        obj = runtime.analyze(text)
         self.assertNotEqual(obj.phrase_matches, [])

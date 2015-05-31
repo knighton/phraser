cae4f01d894ad0603f70df97f67a0dd8249f9fda
Eugene Scherba <escherba@gmail.com>
2015-05-30 23:17:05 -0700

    bump up version to test deployment

--- setup.py
+++ setup.py
@@ -134,7 +134,7 @@ phraser = Extension(
 
 setup(
     name='phraser',
-    version='0.1.6',
+    version='0.1.8',
     author='James Knighton',
     author_email='iamknighton@gmail.com',
     description='Detects phrases in English text',
